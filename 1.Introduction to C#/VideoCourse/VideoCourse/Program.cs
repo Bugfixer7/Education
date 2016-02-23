d0dd3afb1df09f9c4a530772f10d28949f529721
Bugfixer7 <bugfixer7@gmail.com>
2016-02-23 12:50:21 +0200

    1.Introduction to C#
    C# Fundamentals with C# 5.0 VideoCourses

---
+++ 1.Introduction to C#/VideoCourse/VideoCourse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
