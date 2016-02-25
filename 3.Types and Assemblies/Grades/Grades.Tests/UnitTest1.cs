8642a1c723bf2cd5d3acb5163b6ab9880cbf9a83
Bugfixer7 <bugfixer7@gmail.com>
2016-02-25 12:25:59 +0200

    3.Types and Assemblies

---
+++ 3.Types and Assemblies/Grades/Grades.Tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grades;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void CalculatesHighesGrade()
+        {
+            GradeBook book = new GradeBook();
+
+            book.AddGrade(90f);
+            book.AddGrade(50f);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Assert.AreEqual(90f, stats.HighestGrade);
+        }
+
+        [TestMethod]
+        public void PassByValue()
+        {
+            GradeBook book = new GradeBook();
+            book.Name = "Not set";
+            SetName(book);
+
+            Assert.AreEqual("Not set", book.Name);
+        }
+
+        void SetName(GradeBook book)
+        {
+            book.Name = "Name set";
+        }
+    }
+}
